3fc89c85aa2cd5451e834468845e6160040f11ac
smelkin_nikita <nik24sml2000@gmail.com>
2023-04-20 20:12:24 +0300

    самостоятельная работа для практики 5(добавлена быстрая запись микрофона и уровень)

--- app/src/main/java/ru/mirea/smelkin/mireaproject/GyroFragment.java
+++ app/src/main/java/ru/mirea/smelkin/mireaproject/GyroFragment.java
@@ -1,5 +1,10 @@
 package ru.mirea.smelkin.mireaproject;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +12,69 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GyroFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class GyroFragment extends Fragment {
+import ru.mirea.smelkin.mireaproject.databinding.FragmentCameraBinding;
+import ru.mirea.smelkin.mireaproject.databinding.FragmentGyroBinding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class GyroFragment extends Fragment implements SensorEventListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentGyroBinding binding;
 
-    public GyroFragment() {
-        // Required empty public constructor
-    }
+    private TextView horizont_view;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GyroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GyroFragment newInstance(String param1, String param2) {
-        GyroFragment fragment = new GyroFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentGyroBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        horizont_view = binding.horizontView;
+
+        return root;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+
+            int inclination = (int) Math.round(Math.toDegrees(Math.acos(valueRoll)));
+
+            if (inclination < 25 || inclination > 155)
+            {
+                horizont_view.setText("не вертикально");
+            }
+            else
+            {
+                horizont_view.setText("вертикально");
+            }
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gyro, container, false);
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 }
