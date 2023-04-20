3851a4316fbcd314acf313f7b6181d0d47b20f1c
smelkin_nikita <nik24sml2000@gmail.com>
2023-04-20 19:42:57 +0300

    самостоятельная работа для практики 5(добавлена быстрая камера)

--- app/src/main/java/ru/mirea/smelkin/mireaproject/MainActivity.java
+++ app/src/main/java/ru/mirea/smelkin/mireaproject/MainActivity.java
@@ -44,8 +44,9 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        //TODO:объявление страниц
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_musicPlayer)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_musicPlayer, R.id.nav_gyroFragment, R.id.nav_cameraFragment, R.id.nav_audioFragment)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
