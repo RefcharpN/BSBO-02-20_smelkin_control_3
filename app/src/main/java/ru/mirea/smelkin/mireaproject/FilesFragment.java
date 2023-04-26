be6b877c9601e21de1f99a75ab8753fe139b6c8f
smelkin_nikita <nik24sml2000@gmail.com>
2023-04-26 13:45:21 +0300

    самостоятельная работа для практики 6 часть 1(добавлен раздел работы с файлами)

--- app/src/main/java/ru/mirea/smelkin/mireaproject/FilesFragment.java
+++ app/src/main/java/ru/mirea/smelkin/mireaproject/FilesFragment.java
@@ -1,64 +1,200 @@
 package ru.mirea.smelkin.mireaproject;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FilesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import ru.mirea.smelkin.mireaproject.databinding.FragmentFilesBinding;
+import ru.mirea.smelkin.mireaproject.databinding.FragmentProfileBinding;
+
+
 public class FilesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FilesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FilesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FilesFragment newInstance(String param1, String param2) {
-        FilesFragment fragment = new FilesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentFilesBinding binding;
+    private SecretKey key;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        binding = FragmentFilesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialogFragment fragment = new MyDialogFragment();
+                fragment.show(getChildFragmentManager(), "mirea");
+            }
+        });
+
+
+        return root;
+    }
+
+
+
+    public void onOpenClicked() {
+        if(isExternalStorageReadable())
+        {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    readFileFromExternalStorage();
+                }
+            }).start();
+        }
+        else
+        {
+            Toast.makeText(getContext(),"нет разрешения", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(getContext(),"завершено", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSaveClicked() {
+        if(isExternalStorageWritable()) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    writeFileToExternalStorage();
+                }
+            }).start();
+        }
+        else {
+            Toast.makeText(getContext(),"нет разрешения", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(getContext(), "завершено", Toast.LENGTH_SHORT).show();
+    }
+
+    public void readFileFromExternalStorage() {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, String.format("%s.txt",binding.name.getText().toString()));
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file.getAbsoluteFile());
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            String line = reader.readLine();
+            String text = "";
+
+            while (line != null) {
+                text = text + line;
+                line = reader.readLine();
+            }
+
+            SecretKey secretKey=generateKey("any data used as random seed1111");
+            String strResult=decryptMsg(text,secretKey);
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    binding.text.setText(strResult);
+                }
+            });
+
+        } catch (Exception e) {
+            Log.w("ExternalStorage", String.format("Read from file %s failed", e.getMessage()));
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_files, container, false);
+    public void writeFileToExternalStorage() {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, String.format("%s.txt",binding.name.getText().toString()));
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file.getAbsoluteFile());
+            OutputStreamWriter output = new OutputStreamWriter(fileOutputStream);
+
+            SecretKey secretKey=generateKey("any data used as random seed1111");
+            String strResult=encryptMsg(binding.text.getText().toString(),secretKey);
+
+            output.write(strResult);
+            output.close();
+
+        } catch (IOException e) {
+            Log.w("ExternalStorage", "Error writing " + file, e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String encryptMsg(String message, SecretKey secret)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8"));
+        return Base64.encodeToString(cipherText, Base64.NO_WRAP);
+    }
+    public String decryptMsg(String cipherText, SecretKey secret)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidParameterSpecException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        byte[] decode = Base64.decode(cipherText, Base64.NO_WRAP);
+        String decryptString = new String(cipher.doFinal(decode), "UTF-8");
+        return decryptString;
+    }
+    public static SecretKey generateKey(String key)
+            throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        SecretKeySpec secret;
+        secret = new SecretKeySpec(key.getBytes(), "AES");
+        return  secret;
+    }
+
+    /* Проверяем хранилище на доступность чтения и записи*/
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    /* Проверяем внешнее хранилище на доступность чтения */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
